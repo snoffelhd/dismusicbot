d07193756edecff72f68578a8495bc49036dadf2
snoffelhd <32904082+snoffelhd@users.noreply.github.com>
2017-10-20 22:30:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ var bot = require("discord-music-bot");
 var serverName = "🕺 HYPER SERVER 🕺 (Aufbau)";
 var textChannelName = "admim";
 var voiceChannelName = "admim";
-var aliasesFile = "README.ME";
+var aliasesFile = "README.MD";
 var botToken = "MzcwMjAyNTM0NzU0NjQ4MDY0.DMvk0g.dfMFVKZ4R7MZu6jGNZS6Boe8mEY";
  
 bot.run(serverName, textChannelName, voiceChannelName, aliasesFile, botToken);
