be2e244546e0d97b5fefb4c143ce6ce29ad7162c
snoffelhd <32904082+snoffelhd@users.noreply.github.com>
2017-10-20 22:28:06 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,9 @@
+var bot = require("discord-music-bot");
+ 
+var serverName = "🕺 HYPER SERVER 🕺 (Aufbau)";
+var textChannelName = "admim";
+var voiceChannelName = "admim";
+var aliasesFile = "README.ME";
+var botToken = "MzcwMjAyNTM0NzU0NjQ4MDY0.DMvk0g.dfMFVKZ4R7MZu6jGNZS6Boe8mEY";
+ 
+bot.run(serverName, textChannelName, voiceChannelName, aliasesFile, botToken);
